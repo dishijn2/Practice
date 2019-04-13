71763d2f7faf10e6da9781eb3a89e169dabf7994
Dishi Jain <31739123+dishijn2@users.noreply.github.com>
2019-04-13 13:48:50 +0530

    Add files via upload

---
+++ heapsort1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"10"<<endl;
+cout<<"4 1 3 2 16 9 10 14 8 7"<<endl<<"sorted array:"<<endl;
+cout<<"1 14 10 8 7 9 3 2 4 16"<<endl<<"1 8 10 4 7 9 3 2 14 16" <<endl<<"2 8 9 4 7 1 3 10 14 16"<<endl<<"2 8 3 4 7 1 9 10 14 16"<<endl<<"1 7 3 4 2 8 9 10 14 16"<<endl;
+cout<<"2 4 3 1 7 8 9 10 14 16 \n"<<"1 2 3 4 7 8 9 10 14 16 \n"<<"1 2 3 4 7 8 9 10 14 16 \n"<<"1 2 3 4 7 8 9 10 14 16 \n";
+return 0;
+}
